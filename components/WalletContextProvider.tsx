c49f1111d4187750dc35384226d64de8c910fcca
Tanner McMullen <94848716+ilovespectra@users.noreply.github.com>
2023-02-27 07:15:46 -0700

    Update WalletContextProvider.tsx

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -5,7 +5,11 @@ import {
 } from "@solana/wallet-adapter-react"
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 import { clusterApiUrl } from "@solana/web3.js"
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
+import { PhantomWalletAdapter,
+        SolflareWalletAdapter,
+        TorusWalletAdapter, 
+        CoinbaseWalletAdapter 
+} from "@solana/wallet-adapter-wallets"
 import { useMemo } from "react"
 require("@solana/wallet-adapter-react-ui/styles.css")
 
